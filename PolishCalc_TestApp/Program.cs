08e6db996d43d9db548e7c6fd878a8e2e86ad722
Lancepaw <pavelko93@gmail.com>
2022-04-07 10:29:51 +0300

    Initial commit

---
+++ PolishCalc_TestApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using PolishCalcLib;
+
+using static System.Console;
+
+namespace PolishCalc_TestApp{
+static class Program{
+
+static void Main()
+{	double res;
+	string input, errMsg;
+	PolishCalculator calc = new PolishCalculator();
+
+	while( true )
+	{	WriteLine( "Введите математическое выражение в польской записи. Для выхода введите Q" );
+
+		input = ReadLine();
+
+		if( input == "Q" || input == "q" )
+			break;
+
+		if( calc.Calculate( input, out res, out errMsg ) )
+			WriteLine( $"Результат: { res }" ); else
+			WriteLine( errMsg );
+	}
+	
+}
+	
+}}
